2147819491d9a0196916e2876e5a99ad02b6c6bd
Kaio <kposansky@gmail.com>
2025-06-17 00:50:02 -0300

    feat: Repositório de carrinho

---
+++ DaccApi/DaccApi/Model/Carrinhos/StatusCarrinho.cs
@@ -0,0 +1,22 @@
+﻿namespace DaccApi.Model
+{
+    public class StatusCarrinho
+    {
+        private string Value { get; }
+
+        private StatusCarrinho(string value)
+        {
+            this.Value = value;
+        }
+
+        public static readonly StatusCarrinho Ativo = new StatusCarrinho("ativo");
+        public static readonly StatusCarrinho Finalizado = new StatusCarrinho("finalizado");
+        public static readonly StatusCarrinho Abandonado = new StatusCarrinho("abandonado");
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
+
