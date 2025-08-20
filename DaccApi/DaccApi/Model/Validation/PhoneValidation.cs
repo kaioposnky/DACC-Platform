fe499d27e29a9d752e9dcdb0cc2f91f45529b298
Kaio <kposansky@gmail.com>
2025-08-19 21:05:38 -0300

    feat: validação de telefone e RA do usuário
    - Adicinado validação de RA na criação e update de usuário
    - Adicinado validação de Telefone na criação e update de usuário

---
+++ DaccApi/DaccApi/Model/Validation/PhoneValidation.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace DaccApi.Model.Validation
+{
+    public partial class PhoneValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+            var phone = value as string;
+
+            // Validação de telefone, é válido se o número tem 10 ou 11 dígitos ou mais e começa com um número.
+            if (string.IsNullOrWhiteSpace(phone) || PhoneRegex().IsMatch(phone))
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("Número de telefone inválido.");
+        }
+
+        // Formato válido: 11950166444
+        [GeneratedRegex(@"^([1-9]{1})(\d{9,10})$")]
+        private static partial Regex PhoneRegex();
+    }
+}
