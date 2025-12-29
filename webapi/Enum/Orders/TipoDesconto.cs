98e2434ea765f06127a28277fe377f74fa4c8282
Kaio Angelo <kposansky@gmail.com>
2025-12-29 15:44:53 -0300

    feat: adiciona tipo de desconto do cupom

---
+++ webapi/Enum/Orders/TipoDesconto.cs
@@ -0,0 +1,18 @@
+namespace DaccApi.Enum.Posts
+{
+    /// <summary>
+    /// Tipo de desconto do cupom.
+    /// </summary>
+    public enum TipoDesconto
+    {
+        /// <summary>
+        /// Desconto com porcentagem.
+        /// </summary>
+        Percentage,
+        /// <summary>
+        /// Desconto fixo.
+        /// </summary>
+        Fixed
+    }
+
+}
