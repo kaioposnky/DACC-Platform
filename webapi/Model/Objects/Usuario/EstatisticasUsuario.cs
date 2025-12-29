5a1096c16e0506ab49959b36fe9fb4c8016787ea
Kaio Angelo <kposansky@gmail.com>
2025-12-29 16:18:43 -0300

    feat: adiciona média das avaliações do usuário

--- webapi/Model/Objects/Usuario/EstatisticasUsuario.cs
+++ webapi/Model/Objects/Usuario/EstatisticasUsuario.cs
@@ -4,5 +4,6 @@ public class EstatisticasUsuario
 {
     public long Orders { get; set; }
     public long Reviews { get; set; }
+    public decimal AverageRating { get; set; }
     public string RegistryDate { get; set; }
 }
